b7540668377de6938bcf52ca7ad0bfc0af80168f
Ed Sowell <jag_man653@sbcglobal.net>
2019-02-02 11:26:46 -0800

    Put existing Netbeans project into Git repository

---
+++ src/myhelloworld/MyHelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myhelloworld;
+
+/**
+ *
+ * @author jag_m
+ */
+public class MyHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}

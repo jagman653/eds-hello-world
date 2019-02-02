73f06e953aa4b57d6d63b92df45812714fbe6e08
Ed Sowell <jag_man653@sbcglobal.net>
2019-02-02 15:07:11 -0800

    Changed name

--- src/edshelloworld/EdsHelloWorld.java
+++ src/edshelloworld/EdsHelloWorld.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package myhelloworld;
+package edshelloworld;
 
 /**
  *
  * @author jag_m
  */
-public class MyHelloWorld {
+public class EdsHelloWorld {
 
     /**
      * @param args the command line arguments

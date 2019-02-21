0afd04418cb24739336055a73790fd43c1c9f7de
Ed Sowell <jagman653@gmail.com>
2019-02-20 16:38:25 -0800

    First commit after removing .git with Bash

---
+++ src/edshelloworld/ANewClass.java
@@ -0,0 +1,15 @@
+package edshelloworld;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jag_m
+ */
+public class ANewClass {
+    // basically nothing.... just to try things with GitHub
+}
